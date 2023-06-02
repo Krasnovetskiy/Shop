7cc8884d7a76bfb84c1cedf2e136694edd07951b
Krasnovetskiy <valeriykrasnoveckii@gmail.com>
2023-06-02 15:36:35 +0300

    add models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+from os import path
 
-# Create your models here.
+from django.db import models
+
+
+def products_image(instance, filename):
+    _name, extension = path.splitext(filename)
+    return f'products/images/{str(instance.pk)}{extension}'
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to=products_image,
+        null=True
+    )
+    sku = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
